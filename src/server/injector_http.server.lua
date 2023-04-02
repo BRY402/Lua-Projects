181543b3e1a5e3ec88fe8eb2497db58ebedaeb1a
BRY402 <105960244+BRY402@users.noreply.github.com>
2023-04-02 19:08:42 -0300

    Update src/server/injector_http.server.lua

--- src/server/injector_http.server.lua
+++ src/server/injector_http.server.lua
@@ -1,7 +1,7 @@
 local HttpService = game:GetService("HttpService")
 local RunService = game:GetService("RunService")
 if RunService:IsServer() then
-	local func, fail = loadstring(HttpService:GetAsync("https://gist.github.com/BRY402/b81f2790715c4b26686e4196e16fcfdb/raw"), "terminal")
+	local func, fail = loadstring(HttpService:GetAsync("https://gist.github.com/BRY402/b81f2790715c4b26686e4196e16fcfdb/raw"), "injector")
 	if func then
 		func()
 	else
