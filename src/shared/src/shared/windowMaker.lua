3ba53a4c6b90f51a5225f3d98c3f1e95eff4276e
BRY402 <105960244+BRY402@users.noreply.github.com>
2023-04-02 17:21:33 -0300

    Update src/shared/src/shared/windowMaker.lua

--- src/shared/src/shared/windowMaker.lua
+++ src/shared/src/shared/windowMaker.lua
@@ -1,11 +1,22 @@
 local HttpService = game:GetService("HttpService")
 local lib = loadstring(HttpService:GetAsync("https://github.com/BRY402/random-scripts/raw/main/stuff/lib.lua",true), "lib")()
 local ReplicatorRemote = lib.Create("RemoteFunction", owner, {Name = HttpService:GenerateGUID()})
+local types = {
+    "Frame",
+    "Textbox",
+    "Label",
+    "Button",
+    "ImageLabel",
+    "ImageButton"
+}
 local nls_source = [==[local ReplicatorRemote = owner:FindFirstChild(")]==]..ReplicatorRemote.Name..[==[")
 ]==]
 NLS(nls_source, owner:FindFirstChildOfClass("PlayerGui"))
 owner.CharacterAdded:Connect(function()
     NLS(nls_source, owner:FindFirstChildOfClass("PlayerGui"))
 end)
-local window = {}
+local window = {new = function(type_)
+    assert(table.find(types, type_), "Invalid gui type")
+    
+end}
 return window
