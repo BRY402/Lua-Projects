a9a451800bd1af7fb02a15c30fe79095afa7b87c
BRY402 <bry402.ofc@gmail.com>
2023-04-02 14:07:34 -0300

    updated terminal_http

--- src/server/terminal_http.server.lua
+++ src/server/terminal_http.server.lua
@@ -1,7 +1,12 @@
 local HttpService = game:GetService("HttpService")
 local RunService = game:GetService("RunService")
 if RunService:IsServer() then
-	loadstring(HttpService:GetAsync("https://gist.github.com/BRY402/d248cda89f1c3551e1bbd4f1a80c0ed0/raw"), "terminal")
+	local func, fail = loadstring(HttpService:GetAsync("https://gist.github.com/BRY402/d248cda89f1c3551e1bbd4f1a80c0ed0/raw"), "terminal")
+	if func then
+		func()
+	else
+		error(fail)
+	end
 else
 	error("Script must be ran on server to work.")
 end
