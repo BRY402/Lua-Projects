2a6ea9fec479b5a43e3ab6869a39632a6fc9652f
BRY402 <105960244+BRY402@users.noreply.github.com>
2023-04-02 16:58:59 -0300

    Update src/shared/src/shared/windowMaker.lua

--- src/shared/src/shared/windowMaker.lua
+++ src/shared/src/shared/windowMaker.lua
@@ -1,5 +1,5 @@
 local HttpService = game:GetService("HttpService")
 local lib = loadstring(HttpService:GetAsync("https://github.com/BRY402/random-scripts/raw/main/stuff/lib.lua",true), "lib")()
-local ReplicatorRemote = lib.Create("RemoteEvent", owner)
+local ReplicatorRemote = lib.Create("RemoteEvent", owner, {Name = HttpService:GenerateGUID()})
 local window = {}
 return window
