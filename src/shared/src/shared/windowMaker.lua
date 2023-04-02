bbe745a2660839adbc94a99e228caba587ffddc9
BRY402 <105960244+BRY402@users.noreply.github.com>
2023-04-02 17:01:13 -0300

    Update src/shared/src/shared/windowMaker.lua

--- src/shared/src/shared/windowMaker.lua
+++ src/shared/src/shared/windowMaker.lua
@@ -1,7 +1,8 @@
 local HttpService = game:GetService("HttpService")
 local lib = loadstring(HttpService:GetAsync("https://github.com/BRY402/random-scripts/raw/main/stuff/lib.lua",true), "lib")()
 local ReplicatorRemote = lib.Create("RemoteEvent", owner, {Name = HttpService:GenerateGUID()})
-local nls_source = [==[local ReplicatorRemote = owner:FindFirstChild(")]==]..ReplicatorRemote.Name..[==[")]==]
+local nls_source = [==[local ReplicatorRemote = owner:FindFirstChild(")]==]..ReplicatorRemote.Name..[==[")
+]==]
 NLS(nls_source, owner:FindFirstChildOfClass("PlayerGui"))
 owner.CharacterAdded:Connect(function()
     NLS(nls_source, owner:FindFirstChildOfClass("PlayerGui"))
