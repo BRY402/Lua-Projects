f5b4e173e6246845b7bcba2a59570e1d62aed69f
BRY402 <105960244+BRY402@users.noreply.github.com>
2023-04-03 22:50:05 -0300

    Update src/server/stick.server.lua

--- src/server/stick.server.lua
+++ src/server/stick.server.lua
@@ -15,7 +15,7 @@ RunService.PreSimulation:Connect(function(delta)
 			if lastPart then
 				local currentCFrame = lastPart.CFrame
 				local difference = currentCFrame:Inverse() * lastCFrame
-				Character:PivotTo(CFrame.new(difference:ToObjectSpace(HumanoidRootPart.CFrame).Position))
+				Character:PivotTo(HumanoidRootPart.CFrame:ToWorldSpace(currentCFrame))
 				if Camera then
 					Camera.CFrame = difference.Rotation:ToObjectSpace(Camera.CFrame)
 				end
