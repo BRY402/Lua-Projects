c4548b9674b6fc439b4496caec12c44f13f33bb4
BRY402 <105960244+BRY402@users.noreply.github.com>
2023-04-22 21:18:57 -0300

    Update src/shared/ragdoll.lua

--- src/shared/ragdoll.lua
+++ src/shared/ragdoll.lua
@@ -2,7 +2,7 @@ local HttpService = game:GetService("HttpService")
 local lib = loadstring(HttpService:GetAsync("https://github.com/BRY402/random-scripts/raw/main/stuff/lib.lua", true), "lib")()
 local function ragdoll(character)
     local ragdolled = {}
-    local Torso = character:FindFirstChild("Torso")
+    local Torso = character:FindFirstChild("Torso") or character:FindFirstChild("LowerTorso")
     local Humanoid = character:FindFirstChildOfClass("Humanoid")
     assert(Torso, "Missing Torso")
     if Humanoid then
@@ -10,27 +10,14 @@ local function ragdoll(character)
         Humanoid.PlatformStand = true
     end
     local function replace(joint)
-        local part = lib.Create("Part", script, {
-            Transparency = 1,
-            Size = joint.Part1.Size,
-            Massless = true,
-            lib.Create("Weld", nil, {
-                __self = function(weld)
-                    weld:GetPropertyChangedSignal("Parent"):Wait()
-                    weld.Part0 = weld.Parent
-                    weld.Part1 = joint.Part1
-                end
-            })
-        })
         local ball = lib.Create("BallSocketConstraint", script, {
-            Name = joint.Part1.Name,
+            Name = joint.Part1:GetFullName(),
             Attachment0 = lib.Create("Attachment", joint.Part0, {
                 CFrame = joint.C0
             }),
             Attachment1 = lib.Create("Attachment", joint.Part1, {
                 CFrame = joint.C1,
-            }),
-            LimitsEnabled = true
+            })
         })
     end
     local function getJoints(limb)
@@ -48,4 +35,31 @@ local function ragdoll(character)
     end
     getJoints(Torso)
 end
-return ragdoll
+local function unragdoll(character)
+    local ragdolled = {}
+    local Torso = character:FindFirstChild("Torso") or character:FindFirstChild("LowerTorso")
+    local Humanoid = character:FindFirstChildOfClass("Humanoid")
+    assert(Torso, "Missing Torso")
+    if Humanoid then
+        Humanoid.RequiresNeck = false
+        Humanoid.PlatformStand = true
+    end
+    local function getJoints(limb)
+        lib.Loops.read(limb:GetJoints(), function(i, v)
+            if v:IsA("Motor6D") then
+                v.Enabled = true
+                lib.Destroy(script:FindFirstChild(v.Part1:GetFullName(), true))
+                local part1 = v.Part1
+                if table.find(ragdolled, part1) then
+                    table.remove(ragdolled, table.find(ragdolled, part1))
+                    getJoints(part1)
+                end
+            end
+        end)
+    end
+    getJoints(Torso)
+end
+return {
+    Ragdoll = ragdoll,
+    Unragdoll = unragdoll
+}
