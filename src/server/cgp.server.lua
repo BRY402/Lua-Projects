0bd2bdb5db83efc54e8c34999edc097c1cd38ff4
BRY402 <105960244+BRY402@users.noreply.github.com>
2023-05-28 17:57:58 -0300

    Update src/server/cgp.server.lua

--- src/server/cgp.server.lua
+++ src/server/cgp.server.lua
@@ -40,22 +40,23 @@ function Physics:Update()
 	local took = newtime - time
 	time = newtime
 	for i, gui in pairs(Objects) do
+		local velocity = gui:GetAttribute("Velocity")
 		local x, y, sizeX, sizeY = gui.Position.X, gui.Position.Y, gui.Size.X, gui.Size.Y
-		local inbounds1, inbounds2, inbounds3, inbounds4 = y.Offset < Y - sizeY.Offset, y.Offset > -Y + sizeY.Offset, x.Offset < X - sizeX.Offset, x.Offset > -X + sizeX.Offset
+		local inbounds1, inbounds2, inbounds3, inbounds4 = y.Offset < Y - (sizeY.Offset + velocity.Y.Offset), y.Offset > -Y + (sizeY.Offset + velocity.Y.Offset), x.Offset < X - (sizeX.Offset + velocity.X.Offset), x.Offset > -X + (sizeX.Offset + velocity.X.Offset)
 		if inbounds1 and inbounds2 then
-			gui:SetAttribute("Velocity", gui:GetAttribute("Velocity") + UDim2.new(0, 0, 0, Physics.Gravity))
+			gui:SetAttribute("Velocity", velocity + UDim2.new(0, 0, 0, Physics.Gravity))
 			gui.Position = gui.Position + gui:GetAttribute("Velocity")
 		else
-			local y = gui:GetAttribute("Velocity").Y
-			gui:SetAttribute("Velocity", UDim2.new(0, 0, 0, -y.Offset / 2))
+			local y = velocity.Y
+			gui:SetAttribute("Velocity", UDim2.new(0, velocity.X.Offset, 0, -y.Offset * 1.1))
 			gui.Position = gui.Position + gui:GetAttribute("Velocity")
 		end
 		if inbounds3 and inbounds4 then
-			gui:SetAttribute("Velocity", gui:GetAttribute("Velocity") + UDim2.new(0, 1, 0, 0))
+			gui:SetAttribute("Velocity", gui:GetAttribute("Velocity") + UDim2.new(0, 0, 0, 0))
 			gui.Position = gui.Position + gui:GetAttribute("Velocity")
 		else
-			local x = gui:GetAttribute("Velocity").X
-			gui:SetAttribute("Velocity", UDim2.new(0, -x.Offset / 2, 0, 0))
+			local x = velocity.X
+			gui:SetAttribute("Velocity", UDim2.new(0, -x.Offset / 2, 0, velocity.Y.Offset))
 			gui.Position = gui.Position + gui:GetAttribute("Velocity")
 		end
 		if i % 10 == 0 then
