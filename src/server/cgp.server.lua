306782f2fb33350817065605e1417971e2852660
BRY402 <105960244+BRY402@users.noreply.github.com>
2023-05-28 19:42:53 -0300

    Update src/server/cgp.server.lua

--- src/server/cgp.server.lua
+++ src/server/cgp.server.lua
@@ -19,18 +19,22 @@ local gui = lib.Create("SurfaceGui", window, {
 })
 local X, Y = gui.AbsoluteSize.X, gui.AbsoluteSize.Y
 local Objects = {}
-local Obj = {new = function()
-	gui.Parent = owner
-	local newgui = lib.Create("Frame",gui,{Size = UDim2.new(0,100,0,75),
-	BorderSizePixel = 0,
-	Position = UDim2.new(0, 0, 0, 0)})
-	newgui:SetAttribute("Velocity", UDim2.new(0, 0, 0, 0))
-	newgui:SetAttribute("Anchored", false)
-	newgui:SetAttribute("CanCollide", true)
-	table.insert(Objects, newgui)
-	gui.Parent = window
-	return newgui
-end}
+local obj = {
+	new = function()
+		gui.Parent = owner
+		local newgui = lib.Create("Frame", gui, {
+			Size = UDim2.new(0,100,0,75),
+			BorderSizePixel = 0,
+			Position = UDim2.new(0, 0, 0, 0)
+		})
+		newgui:SetAttribute("Velocity", UDim2.new(0, 0, 0, 0))
+		newgui:SetAttribute("Anchored", false)
+		newgui:SetAttribute("CanCollide", true)
+		table.insert(Objects, newgui)
+		gui.Parent = window
+		return newgui
+	end
+}
 local Physics = {Gravity = 1}
 function Physics:IsColliding()
 
@@ -42,30 +46,28 @@ function Physics:Update()
 	for i, gui in pairs(Objects) do
 		local velocity = gui:GetAttribute("Velocity")
 		local x, y, sizeX, sizeY = gui.Position.X, gui.Position.Y, gui.Size.X, gui.Size.Y
-		local inbounds1, inbounds2, inbounds3, inbounds4 = y.Offset < Y - (sizeY.Offset + velocity.Y.Offset), -y.Offset > -Y + (sizeY.Offset + velocity.Y.Offset), x.Offset < X - (sizeX.Offset + velocity.X.Offset), -x.Offset > -X + (sizeX.Offset + velocity.X.Offset)
+		local inbounds1, inbounds2, inbounds3, inbounds4 = y.Offset < Y - (sizeY.Offset + velocity.Y.Offset), y.Offset >= -velocity.Y.Offset, x.Offset < X - (sizeX.Offset + velocity.X.Offset), x.Offset >= -velocity.X.Offset
 		if inbounds1 and inbounds2 then
 			gui:SetAttribute("Velocity", velocity + UDim2.new(0, 0, 0, Physics.Gravity))
-			gui.Position = gui.Position + gui:GetAttribute("Velocity")
 		else
 			local y = velocity.Y
-			gui:SetAttribute("Velocity", UDim2.new(0, velocity.X.Offset, 0, -y.Offset * 1.1))
-			gui.Position = gui.Position + gui:GetAttribute("Velocity")
+			gui:SetAttribute("Velocity", UDim2.new(0, velocity.X.Offset, 0, -y.Offset / 2))
 		end
 		if inbounds3 and inbounds4 then
-			gui:SetAttribute("Velocity", gui:GetAttribute("Velocity") + UDim2.new(0, 0, 0, 0))
-			gui.Position = gui.Position + gui:GetAttribute("Velocity")
+			gui:SetAttribute("Velocity", velocity + UDim2.new(0, 0, 0, 0))
 		else
 			local x = velocity.X
 			gui:SetAttribute("Velocity", UDim2.new(0, -x.Offset / 2, 0, velocity.Y.Offset))
-			gui.Position = gui.Position + gui:GetAttribute("Velocity")
 		end
+		gui.Position = gui.Position + gui:GetAttribute("Velocity")
 		if i % 10 == 0 then
 			task.wait()
 		end
 	end
 	return took
 end
-Obj.new()
+local block = obj.new()
+block:SetAttribute("Velocity", UDim2.new(0, 25, 0, 0))
 while true do
 	task.wait()
 	Physics:Update()
