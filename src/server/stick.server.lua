9d3a4bb6828fca8d4e9675ad70669852746553f0
BRY402 <105960244+BRY402@users.noreply.github.com>
2023-06-06 15:11:37 -0300

    Update src/server/stick.server.lua

--- src/server/stick.server.lua
+++ src/server/stick.server.lua
@@ -10,21 +10,24 @@ RunService.PreSimulation:Connect(function(delta)
 	local Camera = workspace.CurrentCamera
 	if Character then
 		local HumanoidRootPart = Character:FindFirstChild("HumanoidRootPart")
-		if HumanoidRootPart then
-			if lastPart then
-				local currentCFrame = lastPart.CFrame
-				local difference = currentCFrame * lastCFrame:Inverse()
-				HumanoidRootPart.CFrame = difference:ToWorldSpace(HumanoidRootPart.CFrame)
-				if Camera then
-					Camera.CFrame = difference.Rotation:ToWorldSpace(Camera.CFrame)
+		local Humanoid = Character:FindFirstChildOfClass("Humanoid)
+		if HumanoidRootPart and Humanoid then
+			if not Humanoid.SeatPart then
+				if lastPart then
+					local currentCFrame = lastPart.CFrame
+					local difference = currentCFrame * lastCFrame:Inverse()
+					HumanoidRootPart.CFrame = difference:ToWorldSpace(HumanoidRootPart.CFrame)
+					if Camera then
+						Camera.CFrame = difference.Rotation:ToWorldSpace(Camera.CFrame)
+					end
 				end
-			end
-			local result = workspace:Raycast(HumanoidRootPart.Position, Vector3.new(0, -15, 0), params)
-			if result then
-				lastPart = result.Instance
-				lastCFrame = lastPart.CFrame
-			else
-				lastPart = nil
+				local result = workspace:Raycast(HumanoidRootPart.Position, Vector3.new(0, -15, 0), params)
+				if result then
+					lastPart = result.Instance
+					lastCFrame = lastPart.CFrame
+				else
+					lastPart = nil
+				end
 			end
 		end
 	end
