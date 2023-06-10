7ea169e9e592a91dd454283c32b85aebaf0be6b0
BRY402 <105960244+BRY402@users.noreply.github.com>
2023-06-10 15:06:34 -0300

    Update src/server/stick.server.lua

--- src/server/stick.server.lua
+++ src/server/stick.server.lua
@@ -6,32 +6,30 @@ local params = RaycastParams.new()
 params.RespectCanCollide = true
 params.FilterDescendantsInstances = {owner.Character}
 RunService.PreSimulation:Connect(function(delta)
-	local Character = owner.Character
-	local Camera = workspace.CurrentCamera
-	if Character then
-		local HumanoidRootPart = Character:FindFirstChild("HumanoidRootPart")
-		local Humanoid = Character:FindFirstChildOfClass("Humanoid")
-		if HumanoidRootPart and Humanoid then
-			if not Humanoid.SeatPart then
-				if lastPart then
-					local currentCFrame = lastPart.CFrame
-					local difference = currentCFrame * lastCFrame:Inverse()
-					HumanoidRootPart.CFrame = difference:ToWorldSpace(HumanoidRootPart.CFrame)
-					if Camera then
-						Camera.CFrame = difference.Rotation:ToWorldSpace(Camera.CFrame)
-					end
-				end
-				local result = workspace:Raycast(HumanoidRootPart.Position, Vector3.new(0, -15, 0), params)
-				if result then
-					lastPart = result.Instance
-					lastCFrame = lastPart.CFrame
-				else
-					lastPart = nil
-				end
-			end
-		end
-	end
-	params.FilterDescendantsInstances = {owner.Character}
+    local Character = owner.Character
+    local Camera = workspace.CurrentCamera
+    if Character then
+        local HumanoidRootPart = Character:FindFirstChild("HumanoidRootPart")
+        local Humanoid = Character:FindFirstChildOfClass("Humanoid")
+        if HumanoidRootPart and Humanoid and not Humanoid.SeatPart then
+            if lastPart then
+                local currentCFrame = lastPart.CFrame
+                local difference = currentCFrame * lastCFrame:Inverse()
+                HumanoidRootPart.CFrame = difference:ToWorldSpace(HumanoidRootPart.CFrame)
+                if Camera then
+                    Camera.CFrame = difference.Rotation:ToWorldSpace(Camera.CFrame)
+                end
+            end
+            local result = workspace:Spherecast(HumanoidRootPart.Position, 1, Vector3.new(0, -15, 0), params)
+            if result then
+                lastPart = result.Instance
+                lastCFrame = lastPart.CFrame
+            else
+                lastPart = nil
+            end
+        end
+    end
+    params.FilterDescendantsInstances = {owner.Character}
 end)]==]
 NLS(nls_source, owner:FindFirstChildOfClass("PlayerGui"))
 owner.CharacterAdded:Connect(function()
