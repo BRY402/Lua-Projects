2ef44ed9e37d07e003f30cecb85902f76eac0af1
BRY402 <bry402.ofc@gmail.com>
2023-07-15 14:18:48 -0300

    update

--- src/server/btools.server.lua
+++ src/server/btools.server.lua
@@ -1,22 +1,24 @@
 -- note for future self: i could've done this all in the same instance.new but it wouldnt be as readable
 local HttpService = game:GetService("HttpService")
-local lib = loadstring(HttpService:GetAsync("https://github.com/BRY402/random-scripts/raw/main/stuff/lib.lua",true))()
-local tool = lib.Create("Tool",owner.Backpack,true){
-	Name = "Building Tools",
-	ToolTip = "A tool for building",
-	lib.Create("Part",nil,true){
-		Name = "Handle",
-		Material = "SmoothPlastic",
-		Size = Vector3.new(1,1,1)
-	}
-}
-local remote = lib.Create("RemoteFunction",owner.PlayerGui,true){
+local lib = loadstring(HttpService:GetAsync("https://github.com/BRY402/random-scripts/raw/main/stuff/lib.lua", true))()
+local tool = lib.Create("Tool", owner.Backpack, {
+	Name = "Building Tools", 
+	ToolTip = "A tool for building", 
+	lib.Create("Part", nil, {
+		Name = "Handle", 
+		Material = "SmoothPlastic", 
+		Size = Vector3.new(1, 1, 1)
+	})
+})
+local remote = lib.Create("RemoteFunction", owner.PlayerGui, {
 	Name = "SyncRemote_"..HttpService:GenerateGUID()
-}
-remote.OnServerInvoke = function(Player,Data)
+})
+remote.OnServerInvoke = function(Player, Data)
 	if Player == owner then
 		local DataType = Data.SyncType
-		if DataType == "SyncLoaded" then
+		if DataType == "GetAnchored" then
+			return Data.Part.Anchored
+		elseif DataType == "SyncLoaded" then
 			return tool
 		elseif DataType == "SyncMove" then
 			Data.Target.CFrame = Data.CFrame
@@ -34,23 +36,23 @@ local UserInputService = game:GetService("UserInputService")
 local mouse = owner:GetMouse()
 local remote = script.Parent:FindFirstChild("]==]..remote.Name..[==[")
 local tool = remote:InvokeServer({SyncType = "SyncLoaded"})
-local Increments = {Rotate = 90,
-	Move = 1,
+local Increments = {Rotate = 90, 
+	Move = 1, 
 	Resize = 1
 	}
-local Storage = {Mode = "Move",
-	Target = nil,
-	CFrame = CFrame.identity,
-	Size = Vector3.zero,
-	Anchored = false,
-	Saved = {CFrame = CFrame.identity,
+local Storage = {Mode = "Move", 
+	Target = nil, 
+	CFrame = CFrame.identity, 
+	Size = Vector3.zero, 
+	Anchored = false, 
+	Saved = {CFrame = CFrame.identity, 
 		Size = Vector3.zero}
 	}
-local function roundBy(Rounded,Amount)
+local function roundBy(Rounded, Amount)
 	return math.round(Rounded / Amount) * Amount
 end
 local function absVector(Vector)
-	return Vector3.new(math.abs(Vector.X),math.abs(Vector.Y),math.abs(Vector.Z))
+	return Vector3.new(math.abs(Vector.X), math.abs(Vector.Y), math.abs(Vector.Z))
 end
 local function downEvent()
 	Storage.Anchored = Storage.Target.Anchored
@@ -62,15 +64,15 @@ local function downEvent()
 		Storage.Saved.Size = Storage.Target.Size
 	end
 end
-local function dragEvent(Face,Dist)
+local function dragEvent(Face, Dist)
 	if Storage.Mode == "Move" then
 		local NormalId = Vector3.fromNormalId(Face)
-		local Dist = roundBy(Dist,Increments.Move)
+		local Dist = roundBy(Dist, Increments.Move)
 		Storage.CFrame = CFrame.new(NormalId * Dist)
 		Storage.Target.CFrame = Storage.Saved.CFrame * Storage.CFrame
 	elseif Storage.Mode == "Resize" then
 		local NormalId = Vector3.fromNormalId(Face)
-		local Dist = roundBy(Dist,Increments.Resize)
+		local Dist = roundBy(Dist, Increments.Resize)
 		Storage.Size = absVector(NormalId) * Dist
 		Storage.CFrame = CFrame.new((NormalId * Dist) / 2)
 		Storage.Target.Size = Storage.Saved.Size + Storage.Size
@@ -78,13 +80,13 @@ local function dragEvent(Face,Dist)
 	elseif Storage.Mode == "Rotate" then
 		local Dist = math.deg(Dist)
 		local Angle
-		local AngleAxis = math.rad(roundBy(Dist,Increments.Rotate))
+		local AngleAxis = math.rad(roundBy(Dist, Increments.Rotate))
 		if Face == Enum.Axis.X then
-			Angle = CFrame.Angles(AngleAxis,0,0)
+			Angle = CFrame.Angles(AngleAxis, 0, 0)
 		elseif Face == Enum.Axis.Y then
-			Angle = CFrame.Angles(0,AngleAxis,0)
+			Angle = CFrame.Angles(0, AngleAxis, 0)
 		elseif Face == Enum.Axis.Z then
-			Angle = CFrame.Angles(0,0,AngleAxis)
+			Angle = CFrame.Angles(0, 0, AngleAxis)
 		end
 		Storage.CFrame = Angle
 		Storage.Target.CFrame = Storage.Saved.CFrame * Storage.CFrame
@@ -92,28 +94,28 @@ local function dragEvent(Face,Dist)
 end
 local function sync()
 	if Storage.Mode == "Move" then
-		remote:InvokeServer({SyncType = "SyncMove",
-			Target = Storage.Target,
+		remote:InvokeServer({SyncType = "SyncMove", 
+			Target = Storage.Target, 
 			CFrame = Storage.Saved.CFrame * Storage.CFrame})
 	elseif Storage.Mode == "Resize" then
-		remote:InvokeServer({SyncType = "SyncResize",
-			Target = Storage.Target,
-			CFrame = Storage.Saved.CFrame * Storage.CFrame,
+		remote:InvokeServer({SyncType = "SyncResize", 
+			Target = Storage.Target, 
+			CFrame = Storage.Saved.CFrame * Storage.CFrame, 
 			Size = Storage.Saved.Size + Storage.Size})
 	elseif Storage.Mode == "Rotate" then
-		remote:InvokeServer({SyncType = "SyncRotate",
-			Target = Storage.Target,
+		remote:InvokeServer({SyncType = "SyncRotate", 
+			Target = Storage.Target, 
 			CFrame = Storage.Saved.CFrame * Storage.CFrame})
 	end
 end
 local function upEvent()
-	Storage.Target.Anchored = Storage.Anchored
+	Storage.Target.Anchored = remote:InvokeServer({SyncType = "GetAnchored", Part = Storage.Target})
 	sync()
 end
-local guis = Instance.new("Folder",nil)
-local handles = Instance.new("Handles",guis)
-local archandles = Instance.new("ArcHandles",guis)
-local selection = Instance.new("SelectionBox",guis)
+local guis = Instance.new("Folder", nil)
+local handles = Instance.new("Handles", guis)
+local archandles = Instance.new("ArcHandles", guis)
+local selection = Instance.new("SelectionBox", guis)
 guis.Name = "HandleGuis"
 handles.MouseButton1Down:Connect(downEvent)
 handles.MouseButton1Up:Connect(upEvent)
@@ -166,7 +168,7 @@ mouse.Button1Down:Connect(function()
 		end
 	end
 end)
-UserInputService.InputBegan:Connect(function(Key,Processed)
+UserInputService.InputBegan:Connect(function(Key, Processed)
 	if not Processed then
 		if Key.KeyCode == Enum.KeyCode.Z then
 			changeMode("Move")
@@ -182,4 +184,4 @@ tool.Equipped:Connect(function()
 end)
 tool.Unequipped:Connect(function()
 	guis.Parent = nil
-end)]==],owner.PlayerGui)
+end)]==], owner.PlayerGui)
